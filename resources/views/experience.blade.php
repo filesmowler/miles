9bd24c5cf653e87b2de8f0cbb3c6cc6e6995f085
filesmowler <filesmowler@gmail.com>
2017-04-02 20:14:31 -0400

    Experience Column Finished
    Finished adding the rest of the jobs and their details under the
    experience column. Still need to figure out how I want to layout the
    webpage as a whole.

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -15,3 +15,17 @@
 					<li>Organized car lot</li>
 					<li>Minor janitorial duties</li>
 				</ul>
+		<h3><strong>Mr. Pita/Papa Romano's</strong></h3>
+			<I>November 2014 - May 2015</I><br>
+			<I>Delivery Driver/In-store</I></br>
+				<ul>
+					<li>Normal delivery and also catering to residential and commercial</li>
+					<li>In-store duties (clean dishes, stock coolers, fold boxes, handle register, answer phones, janitorial duties)</li>
+				</ul>
+		<h3><strong>Jets Pizza</strong></h3>
+			<I>August 2011 - October 2014</I><br>
+			<I>Delivery Driver</I></br>
+				<ul>
+					<li>Delivered orders to customers, both residential and commercial</li>
+					<li>Simple In-store duties (stocking and rotating the cooler, folding boxes, etc)</li>
+				</ul>
